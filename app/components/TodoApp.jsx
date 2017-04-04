a453068ce178610d9ffa243ddd182f8884722756
Adam Thompson <Adam@Adams-MacBook-Pro.local>
2017-04-04 21:06:35 +1000

    Various changes

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -3,9 +3,15 @@ const React = require('react');
 var TodoApp = React.createClass({
     render: function () {
         return (
-            <div>
-                Sorry I can't hang tonight
+            <div className="row">
+                <div className="column small-centered">
+                    <h1 className="sorry">Sorry I can't hang tonight</h1>
+                </div>
+                <div className="column small-centered">
+                    <img className="gong" src="https://media.giphy.com/media/AHfqbsP51mMQE/giphy.gif" alt="gonnnng"/>
+                </div>
             </div>
+            
         );
     }
 });
