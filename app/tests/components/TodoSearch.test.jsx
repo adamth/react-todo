fb982471375c0c249c218676aa7af4525a173a8c
Adam Thompson <Adam@Adams-MacBook-Pro.local>
2017-04-05 22:05:08 +1000

    Add TodoSearch tests

---
+++ app/tests/components/TodoSearch.test.jsx
@@ -0,0 +1,36 @@
+const React         = require('react');
+const ReactDOM      = require('react-dom');
+const expect        = require('expect');
+const $             = require('jQuery');
+const testUtils     = require('react-addons-test-utils');
+const TodoSearch    = require('TodoSearch');
+
+describe('TodoSearch',() => {
+    it('should exist', () => {
+        expect(TodoSearch).toExist();
+    });
+
+    it('should call onSearch with entered input text', () => {
+        var spy = expect.createSpy();
+        var searchText = 'Dog';
+        var todoSearch = testUtils.renderIntoDocument(<TodoSearch onSearch={spy} />);
+
+        todoSearch.refs.searchText.value = searchText;
+
+        testUtils.Simulate.change(todoSearch.refs.searchText);
+
+        expect(spy).toHaveBeenCalledWith(false, searchText);
+    });
+
+    it('should call onSeach when showCompleted is checked', () => {
+        var spy = expect.createSpy();
+        var todoSearch = testUtils.renderIntoDocument(<TodoSearch onSearch={spy} />);
+
+        todoSearch.refs.showCompleted.checked = true;
+
+        testUtils.Simulate.change(todoSearch.refs.showCompleted);
+
+        expect(spy).toHaveBeenCalledWith(true, '');
+    });
+    
+});
