35eee203d557877fa94a9c3ac8acbba584e169e6
Adam Thompson <Adam@Adams-MacBook-Pro.local>
2017-04-08 22:18:31 +1000

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -21,5 +21,37 @@ module.exports = {
 
         return $.isArray(todos) ? todos : [];
 
+    },
+    filterTodos: function (todos, showCompleted, searchText) {
+        var filteredTodos = todos;
+
+        filteredTodos = filteredTodos.filter((todo) => {
+            return !todo.completed || showCompleted;
+        });
+
+        filteredTodos = filteredTodos.filter((todo) => {
+            if(!searchText){
+                return true;
+            }
+            if(todo.text.toLowerCase().indexOf(searchText.toLowerCase()) >= 0)
+            {
+                return true;
+            }
+            return false;
+        });
+
+        filteredTodos.sort((a, b) => {
+            if(!a.completed && b.completed)
+            {
+                return -1;
+            } else if (a.completed && !b.completed)
+            {
+                return 1;
+            } else {
+                return 0;
+            }
+        });
+
+        return filteredTodos;
     }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -50,6 +50,47 @@ describe('TodoAPI', () => {
             localStorage.setItem('todos', JSON.stringify(todos));
             var actualTodos = TodoAPI.getTodos();
             expect(actualTodos).toEqual(todos);
-        })
+        });
+    });
+
+    describe('filterTodos',() => {
+        var todos = [
+            {
+                id: 1,
+                text: 'Some text',
+                completed: true
+            },
+            {
+                id: 2,
+                text: 'Other text',
+                completed: false
+            },
+            {
+                id: 3,
+                text: 'Even more text',
+                completed: true
+            },
+        ]
+
+        it('should return all items if show completed is true', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos.length).toBe(3);
+        });
+
+        it('should return todos that have not been completed', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+            expect(filteredTodos.length).toBe(1);
+        });
+
+        it('should sort by completed status', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+            expect(filteredTodos[0].completed).toBe(false);
+        });
+
+        it('should return todos with matching text', () => {
+            var filteredTodos = TodoAPI.filterTodos(todos, true, 'Even');
+            expect(filteredTodos.length).toBe(1);
+        });
+
     });
 });
