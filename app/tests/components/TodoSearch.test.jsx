4c3027ff051460a7f5f2d84fb45632e643d18797
Adam Thompson <Adam@Adams-MacBook-Pro.local>
2017-04-11 22:52:44 +1000

    Update test-utils to load using new path

--- app/tests/components/TodoSearch.test.jsx
+++ app/tests/components/TodoSearch.test.jsx
@@ -2,7 +2,7 @@ const React         = require('react');
 const ReactDOM      = require('react-dom');
 const expect        = require('expect');
 const $             = require('jQuery');
-const testUtils     = require('react-addons-test-utils');
+const testUtils     = require('react-dom/test-utils');
 const TodoSearch    = require('TodoSearch');
 
 describe('TodoSearch',() => {
