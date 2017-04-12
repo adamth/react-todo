12414e5451d2f74e899f8cc7d8d5cad4ed7cad50
adamth <adam@adamth.com>
2017-04-12 16:55:27 +1000

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -9,6 +9,15 @@ var {
 
 var TodoApp = require('TodoApp');
 
+var actions = require('actions');
+var store = require('configureStore').configure();
+
+store.subscribe(() => {
+    console.log('New state', store.getState());
+});
+
+store.dispatch(actions.addTodo('Node projects'));
+
 // Load foundation
 $(document).foundation();
 
---
+++ app/store/configureStore.jsx
@@ -0,0 +1,17 @@
+const redux = require('redux');
+
+const {searchTextReducer, showCompletedReducer, todoReducer} = require('reducers');
+
+export var configure = () => {
+    var reducer = redux.combineReducers({
+        searchText: searchTextReducer,
+        showCompleted: showCompletedReducer,
+        todos: todoReducer
+    });
+
+    var store = redux.createStore(reducer, redux.compose(
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    ));
+
+    return store;
+};
